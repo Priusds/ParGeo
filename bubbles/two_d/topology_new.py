14cb090e7c2fb250bc8e0d16e7b3b52c8f312923
Gruhlke <gruhlke@dirichlet.imp.fu-berlin.de>
2024-01-18 18:05:48 +0100

    new plot logic with comparison testing

--- bubbles/two_d/topology_new.py
+++ bubbles/two_d/topology_new.py
@@ -9,6 +9,22 @@ from bubbles.two_d.hole import Rectangular
 from shapely.affinity import translate as shapely_translate
 
 
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+import matplotlib.patches as mpatches
+from matplotlib.lines import Line2D
+
+class Subdomain(BaseModel):
+    polygon: shapely.Polygon | shapely.MultiPolygon
+    level: int
+    is_hole: bool
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    # def __hash__(self) -> int:
+    #     return self.polygon.__hash__()
+
 
 class Topology:
     DEFAULT_GRID_SIZE = 1e-15
@@ -101,9 +117,6 @@ class Topology:
 
         return True
     
-
-
-
     def __is_valid_level(self, level, is_hole):
 
         if level == 0: 
@@ -128,7 +141,6 @@ class Topology:
                     "Given level was already associated with hole and cannot be used for an inclusion."
                 )
 
-
     def __update_topology(self, polygon, level, is_hole, topology, merge=False):
     
         if not merge:
@@ -142,3 +154,394 @@ class Topology:
                 self.__topology[level] = shapely.union(
                     [self.__topology[level], polygon]
                 )
+
+    def plot(self): 
+        pass
+
+
+def plot(bubbles, level2is_hole, ref_domain) -> Any:
+    """
+    Plot the reference domain and the bubbles.
+    """
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import Normalize
+    import matplotlib.patches as mpatches
+    from matplotlib.lines import Line2D
+
+    bubbles = list(bubbles)
+    bubbles.sort(key=lambda x: x.level)
+
+    hole_boundary_color = "orange"
+    ref_boundary_color = "black"
+    ref_domain_color = "silver"
+    ref_domain_hole_color = "white"
+
+    # level2is_hole = self.level2is_hole TODO: Remove this
+
+    # Filter out levels that are not represented by any bubble.
+    # This is necessary as previous added bubbles might have been removed.
+    present_levels = set([bubble.level for bubble in bubbles])
+    level2is_hole = {
+        lvl: is_hole for lvl, is_hole in level2is_hole.items() if lvl in present_levels
+    }
+    levels = set(level2is_hole.keys())
+
+    # Create a colormap for the levels.
+    lvl2cl = dict()
+    if 0 in levels:
+        lvl2cl[0] = ref_domain_color
+        levels.remove(0)
+    if len(levels) > 0:
+        norm = Normalize(vmin=min(levels), vmax=max(levels))
+        for lvl in levels:
+            lvl2cl[lvl] = plt.cm.cool(norm(lvl))
+    # Make a legend.
+    handles = [
+        Line2D([0], [0], label="Reference boundary", color=ref_boundary_color),
+        Line2D([0], [0], label="Hole boundary", color=hole_boundary_color),
+    ]
+    for lvl, cl in lvl2cl.items():
+        if lvl == 0:
+            label_text = f"Domain lvl = {lvl}"
+        else:
+            label_text = (
+                f"Hole lvl = {lvl}" if level2is_hole[lvl] else f"Inclusion lvl = {lvl}"
+            )
+        handles.append(mpatches.Patch(color=cl, label=label_text))
+    plt.legend(handles=handles)
+
+    # Plot the reference domain.
+    x, y = ref_domain.polygon.exterior.xy
+    plt.plot(x, y, color=ref_boundary_color)
+    plt.fill(x, y, color=ref_domain_color)
+    for pp in ref_domain.polygon.interiors:
+        x, y = pp.xy
+        plt.plot(x, y, color=ref_boundary_color)
+        plt.fill(x, y, color=ref_domain_hole_color)
+
+    # Plot the bubbles
+    for bubble in bubbles:
+        # Filter out reference domain
+        if bubble.level != 0:
+            x, y = bubble.polygon.exterior.xy
+            plt.fill(x, y, color=lvl2cl[bubble.level])
+            if bubble.is_hole:
+                plt.plot(x, y, color=hole_boundary_color)
+
+    # Re-Plot Bubbles without holes
+    for bubble in bubbles:
+        if len(bubble.polygon.interiors) == 0:
+            x, y = bubble.polygon.exterior.xy
+            plt.fill(x, y, color=lvl2cl[bubble.level])
+            if bubble.is_hole:
+                plt.plot(x, y, color=hole_boundary_color)
+
+    # Show the plot
+    plt.show()
+
+
+def plot2(bubbles, level2is_hole) -> Any:
+    """
+    Plot the reference domain and the bubbles.
+    """
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import Normalize
+    import matplotlib.patches as mpatches
+    from matplotlib.lines import Line2D
+
+    bubbles.sort(key=lambda x: x.level)
+
+    hole_boundary_color = "orange"
+    ref_boundary_color = "black"
+    ref_domain_color = "silver"
+    ref_domain_hole_color = "white"
+
+    # level2is_hole = self.level2is_hole TODO: Remove this
+
+    # Filter out levels that are not represented by any bubble.
+    # This is necessary as previous added bubbles might have been removed.
+    present_levels = set([bubble.level for bubble in bubbles])
+    level2is_hole = {
+        lvl: is_hole for lvl, is_hole in level2is_hole.items() if lvl in present_levels
+    }
+    levels = set(level2is_hole.keys())
+
+    # Create a colormap for the levels.
+    lvl2cl = dict()
+    if 0 in levels:
+        lvl2cl[0] = ref_domain_color
+        levels.remove(0)
+    if len(levels) > 0:
+        norm = Normalize(vmin=min(levels), vmax=max(levels))
+        for lvl in levels:
+            lvl2cl[lvl] = plt.cm.cool(norm(lvl))
+
+    # Plot the bubbles
+    for bubble in bubbles:
+        x, y = bubble.polygon.exterior.xy
+        plt.fill(x, y, color=lvl2cl[bubble.level])
+        if len(bubble.polygon.interiors) > 0:
+            for pp in bubble.polygon.interiors:
+                x, y = pp.xy
+                plt.fill(x, y, color="white")
+
+    # Re-Plot Bubbles without holes
+    for bubble in bubbles:
+        if len(bubble.polygon.interiors) == 0:
+            x, y = bubble.polygon.exterior.xy
+            plt.fill(x, y, color=lvl2cl[bubble.level])
+            if bubble.is_hole:
+                plt.plot(x, y, color=hole_boundary_color)
+
+    # Show the plot
+    plt.show()
+
+
+
+def generate_topo():
+    from bubbles.two_d.hole import Circle, Rectangular, Stellar, Ellipse
+
+    from bubbles.two_d.topology import Topology as Topology_Working
+    from bubbles.two_d.topology import Bubble
+    import math
+    """Generate a topology with a few edge cases."""
+    # Make the domain
+    d_1 = shapely.Polygon(
+        Rectangular(midpoint=(0.0, 0), width=2, height=2).discretize_hole(refs=4)
+    )
+    d_2 = shapely.Polygon(
+        Circle(midpoint=(1.0, 0), radius=0.5).discretize_hole(refs=50)
+    )
+
+    dh_1 = shapely.Polygon(
+        Circle(midpoint=(0.0, 0.75), radius=0.2).discretize_hole(refs=50)
+    )
+
+    dh_2 = shapely.Polygon(
+        Circle(midpoint=(0.0, 0.0), radius=0.2).discretize_hole(refs=50)
+    )
+    dh_3 = shapely.Polygon(
+        Ellipse(midpoint=(0.8, 0.0), axis=(0.05, 0.3), angle=0).discretize_hole(refs=50)
+    )
+    domain = d_1
+    domain = domain.union(d_2)
+    #domain = domain.difference(dh_1)
+    #domain = domain.difference(dh_2)
+    #domain = domain.difference(dh_3)
+    topo = Topology_Working(domain)
+    #topo.add(
+    #    bubble=Bubble(polygon=domain, level=0, is_hole=False)
+    #)
+    
+    # Add bubbles
+    # c_1 = shapely.Polygon(
+    #     Circle(midpoint=(-0.25, 0.0), radius=0.25).discretize_hole(refs=50)
+    # )
+    # topo.add(Bubble(polygon=c_1, level=10, is_hole=True))
+    
+    
+    # c_2 = shapely.Polygon(
+    #     Circle(midpoint=(-0.1, 0.4), radius=0.3).discretize_hole(refs=50)
+    # )
+    # topo.add(Bubble(polygon=c_2, level=11, is_hole=False))
+    
+
+    # c_3 = shapely.Polygon(
+    #     Circle(midpoint=(0.75, 0), radius=0.25).discretize_hole(refs=50)
+    # )
+    # topo.add(Bubble(polygon=c_3, level=1, is_hole=False))
+    
+
+    # c_4 = shapely.Polygon(
+    #     Circle(midpoint=(1.0, 0), radius=0.25).discretize_hole(refs=50)
+    # )
+    # topo.add(Bubble(polygon=c_4, level=1, is_hole=False))
+    
+    # # this guy is intersection with the domain
+    # e_1 = shapely.Polygon(
+    #     Ellipse(
+    #         midpoint=(-0.5, 0.75), axis=(1, 0.1), angle=0.25 * math.pi
+    #     ).discretize_hole(refs=200)
+    # )
+    # topo.add(Bubble(polygon=e_1, level=1, is_hole=False))
+    
+
+    # s_1 = shapely.Polygon(
+    #     Stellar(midpoint=(-0.75, -0.6), radius=0.2).discretize_hole(refs=100)
+    # )
+    # topo.add(Bubble(polygon=s_1, level=1, is_hole=False))
+
+    # s_2 = shapely.Polygon(
+    #     Stellar(midpoint=(-0.5, -0.6), radius=0.25).discretize_hole(refs=100)
+    # )
+    # topo.add(Bubble(polygon=s_2, level=1, is_hole=False))
+    
+
+    # c_5 = shapely.Polygon(
+    #     Circle(midpoint=(0.5, 0.6), radius=0.2).discretize_hole(refs=50)
+    # )
+    # topo.add(Bubble(polygon=c_5, level=3, is_hole=False))
+
+    # c_6 = shapely.Polygon(
+    #     Circle(midpoint=(0.6, 0.6), radius=0.2).discretize_hole(refs=50)
+    # )
+    # topo.add(Bubble(polygon=c_6, level=2, is_hole=False))
+    
+    c_7 = shapely.Polygon(
+        Circle(midpoint=(0.24, -0.8), radius=0.22).discretize_hole(refs=50)
+    )
+    topo.add(Bubble(polygon=c_7, level=1, is_hole=False))
+    
+    is_hole = False
+    level = 3
+    e_2 = shapely.Polygon(
+        Ellipse(midpoint=(0.25, -0.6), axis=(0.05, 0.2), angle=0.0).discretize_hole(
+            refs=50
+        )
+    )
+    topo.add(Bubble(polygon=e_2, level=level, is_hole=is_hole))
+
+    e_3 = shapely.Polygon(
+        Ellipse(midpoint=(0.4, -0.4), axis=(0.2, 0.05), angle=0.0).discretize_hole(
+            refs=50
+        )
+    )
+    topo.add(Bubble(polygon=e_3, level=level, is_hole=is_hole))
+
+    e_4 = shapely.Polygon(
+        Ellipse(midpoint=(0.5, -0.6), axis=(0.05, 0.2), angle=0.0).discretize_hole(
+            refs=50
+        )
+    )
+    topo.add(Bubble(polygon=e_4, level=level, is_hole=is_hole))
+
+    # this guy encloses the loop !
+    e_5 = shapely.Polygon(
+        Ellipse(midpoint=(0.4, -0.7), axis=(0.2, 0.05), angle=0.0).discretize_hole(
+            refs=50
+        )
+    )
+    topo.add(Bubble(polygon=e_5, level=level, is_hole=is_hole))
+
+    # c_8 = shapely.Polygon(
+    #     Circle(midpoint=(-0.5, 0.4), radius=0.45).discretize_hole(refs=50)
+    # )
+    # topo.add(Bubble(polygon=c_8, level=3, is_hole=False))
+
+    # c_9 = shapely.Polygon(
+    #     Circle(midpoint=(0.0, 0.75), radius=0.01).discretize_hole(refs=50)
+    # )
+
+    # topo.add(Bubble(polygon=c_9, level=3, is_hole=False))
+
+    return topo
+
+
+def test_sorting():
+        from functools import cmp_to_key
+ 
+        def polygon_compare(poly_with_id_1, poly_with_id_2):
+            polygon_1, id_1 = poly_with_id_1
+            polygon_2, id_2 = poly_with_id_2
+
+            if len(polygon_1.interiors) > 0 : 
+                for pp in polygon_1.interiors :
+                    if pp.contains(polygon_2): 
+                        return -1
+                            
+            if len(polygon_2.interiors) > 0 : 
+                for pp in polygon_2.interiors :
+                    if pp.contains(polygon_1): 
+                        return 1 
+
+            return id_1 < id_2 ###
+
+        compare_key = cmp_to_key(polygon_compare)
+
+        def polygon_compare_simple(poly_1, poly_2):
+            polygon_1, _ = poly_1
+            polygon_2, _ = poly_2
+
+            polygon_1 = shapely.Polygon(polygon_1.exterior)
+            polygon_2 = shapely.Polygon(polygon_2.exterior)
+            if polygon_1.contains(polygon_2):
+                return 1
+            elif polygon_2.contains(polygon_1):
+                return -1
+            # if len(polygon_1.interiors) > 0 : 
+            #     for pp in polygon_1.interiors :
+            #         if pp.contains(polygon_2): 
+            #             return -1
+            # if len(polygon_2.interiors) > 0 : 
+            #     for pp in polygon_2.interiors :
+            #         if pp.contains(polygon_1): 
+            #             return 1 
+            return 0
+
+
+        return polygon_compare_simple
+        
+
+
+        
+
+
+
+if __name__ == "__main__":
+    from functools import cmp_to_key
+    topo = generate_topo()
+
+    # print(topo.bubbles)
+
+    #topo.plot()
+    #exit()
+
+    flattened_polygons = [ (bubble.polygon, bubble.level) for bubble in topo.bubbles]
+
+    polygon_compare_simple = test_sorting()
+
+
+
+
+
+    
+    sorted_bubbles = sorted(flattened_polygons, key = cmp_to_key(polygon_compare_simple), reverse=True)
+
+    for k, A_t in enumerate(sorted_bubbles):
+        for  l, B_t in enumerate(sorted_bubbles):
+            if k > l : 
+                A, lvlA = A_t
+                B, lvlB = B_t
+                print(f"{lvlA} : {lvlB} -> {polygon_compare_simple(A_t,B_t) }")
+                print(f" A has interior : {len(A.interiors)}")
+                print(f" B has interior : {len(B.interiors)}")
+                print("================================================")
+
+
+    #exit()
+
+    #hole_boundary_color = "orange"
+    #ref_boundary_color = "black"
+    ref_domain_color = "silver"
+    ref_domain_hole_color = "white"
+    levels = topo.levels
+
+    # Create a colormap for the levels.
+    lvl2cl = dict()
+    if 0 in levels:
+        lvl2cl[0] = ref_domain_color
+        levels.remove(0)
+    if len(levels) > 0:
+        norm = Normalize(vmin=min(levels), vmax=max(levels))
+        for lvl in levels:
+            lvl2cl[lvl] = plt.cm.cool(norm(lvl))
+
+    for polygon, level in sorted_bubbles: 
+        x, y = polygon.exterior.xy
+        plt.fill(x, y, color=lvl2cl[level])
+        if len(polygon.interiors) > 0:
+            for pp in polygon.interiors:
+                x, y = pp.xy
+                plt.fill(x, y, color="white")
+                
+    plt.show()
