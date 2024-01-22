caaab5242e9124942ae6e83bb6a647baef5337ee
Gruhlke <gruhlke@dirichlet.imp.fu-berlin.de>
2024-01-22 23:27:43 +0100

    updating example for transform

--- examples/two_d/stellar_samples_periodic_constrains.py
+++ examples/two_d/stellar_samples_periodic_constrains.py
@@ -92,31 +92,6 @@ def generate_topo():
 
     return topo
 
-# def generate_topo():
-#     """Generate a rectangular topology with many stellar inclusions."""
-#     domain = shapely.Polygon(
-#         Rectangular(midpoint=(0.5, 0.5), width=1, height=1).discretize_hole(refs=4)
-#     )
-#     topo = Topology(domain)
-
-#     n_stellar = 20
-#     # random.seed(0)
-#     levels = [random.choice([1, 2, 3, 4, 5]) for _ in range(n_stellar)]
-#     midpoints = [(random.random(), random.random()) for _ in range(n_stellar)]
-#     radii = [min((0.09, random.random() * 0.5)) for _ in range(n_stellar)]
-
-#     for mid, rad, lvl in zip(midpoints, radii, levels):
-#         C = shapely.Polygon(Stellar(midpoint=mid, radius=rad).discretize_hole(refs=50))
-#         topo.add(
-#             polygon=C,
-#             level=lvl,
-#             transform=lambda x: clip_x(x, 0, 1),
-#             constraints=(lambda x: polygon_distance_req(x, topo.topology[1], 0.1)) if 1 in topo.topology else None,
-#         )
-
-#     return topo
-
-
 if __name__ == "__main__":
     topo = generate_topo() #generate_topo_simple() #generate_topo()
     topo.set_holes({2})
