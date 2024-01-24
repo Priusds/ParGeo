d61d6eb590e4c14afbc52aa02c415b9cd39692af
TillS <tillschaefer96@hotmail.it>
2024-01-24 17:59:39 +0100

    refactoring

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -1,21 +1,9 @@
+"""Unit tests for the topology module."""
 from bubbles.geometry import Circle, Rectangle
-from bubbles.gmsh_utils import topology_to_gmsh_entities, write_geo
 from bubbles.topology import Topology
 
 
-def test_general():
-    # TODO: Add tests
-    # Create a topology
-    topo = Topology(
-        domain=Rectangle(midpoint=(0, 0), width=1, height=1).discretize(), holes={1}
-    )
-
-    # Add a circle
-    circle = Circle(midpoint=(0.5, 0.5), radius=0.2).discretize(refs=10)
-    topo.add(polygon=circle, level=1)
-
-
-def test_all():
+def test_simple():
     domain = Rectangle(midpoint=(0, 0), width=4, height=4).discretize()
     topo = Topology(domain=domain)
 
@@ -24,11 +12,3 @@ def test_all():
 
     topo.add(polygon=c, level=2)
     topo.add(polygon=r, level=1)
-    topo.plot()
-    gmsh_entities = topology_to_gmsh_entities(topo)
-    write_geo(
-        gmsh_entities=gmsh_entities, file_name="chess_board", correct_curve_loops=True
-    )
-
-
-test_all()
