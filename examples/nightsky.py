d6298409f5edd20d391c744a222f0d64e074ae47
Gruhlke <gruhlke@dirichlet.imp.fu-berlin.de>
2024-01-25 01:19:18 +0100

    periodic examples (circular chessboard, football)  + nightsky examples (stars, raindops)

---
+++ examples/nightsky.py
@@ -0,0 +1,57 @@
+from bubbles.gmsh_utils import topology_to_gmsh_entities, write_geo
+from bubbles.geometry import Rectangle, Stellar, Circle, NStar, RainDrop
+from bubbles.topology import Topology
+from bubbles.transform import Periodic
+from bubbles.constraint import DistanceConstraint 
+
+import random
+
+import math
+
+def generate_topo():
+    domain = Rectangle(midpoint=(0., 0.), width=2, height=1).to_polygon()
+    topo = Topology(domain)
+
+    random.random()
+
+    n_stars = 100
+    # random.seed(0)
+    midpoints = [(2*random.random()-1, random.random()-0.5) for _ in range(n_stars)]
+    alphas = [ 2*math.pi*random.random()  for _ in range(n_stars)]
+
+    constraint = DistanceConstraint()
+    constraint.set_level_distance(0.01, "any", "any")
+
+    radii_in = [ 0.04 * random.random()+0.005  for _ in range(n_stars)]
+    radii_out = [ 2*r_in for r_in in radii_in]
+
+    Ns = [random.choice([5, 6, 7]) for _ in range(n_stars)]
+    
+    for M, r_in, r_out,N,  alph in zip(midpoints, radii_in, radii_out,Ns,  alphas):
+        star = NStar(M, r_in, r_out, N, alph).to_polygon()
+        topo.add(star, level = 1, constraint=constraint)
+
+
+    n_rain = 200
+    midpoints = [(2*random.random()-1, random.random()-0.5) for _ in range(n_rain)]
+    A = [ 0.2 * random.random()+ 0.6 for _ in range(n_rain)]
+    S = [ 0.001 * random.random()+0.005 for _ in range(n_rain)]
+    for M, a, scale in zip(midpoints, A, S): 
+        raindrop = RainDrop(M,a, scale).discretize(128)
+        topo.add(raindrop, level = 2, constraint=constraint)
+
+    
+
+
+    return topo
+
+
+if __name__ == "__main__":
+    topo = generate_topo()  # generate_topo_simple() #generate_topo()
+    topo.plot()
+    # gmsh_entities = topology_to_gmsh_entities(topo)
+    # write_geo(
+    #     gmsh_entities=gmsh_entities,
+    #     file_name="stellar_samples_periodic_constrains",
+    #     correct_curve_loops=True,
+    # )
---
+++ examples/periodic_examples.py
@@ -0,0 +1,57 @@
+import random
+from bubbles.gmsh_utils import topology_to_gmsh_entities, write_geo
+from bubbles.geometry import Rectangle, Stellar, Circle
+from bubbles.topology import Topology
+from bubbles.transform import Periodic
+
+
+def generate_topo():
+    domain = Circle(midpoint=(0., 0.), radius = 1).discretize(refs=50)
+    topo = Topology(domain)
+
+    r11 = Rectangle(midpoint=(0., 0.), width=0.05, height=0.05).to_polygon()
+    r12 = Rectangle(midpoint=(0.05, 0.05), width=0.05, height=0.05).to_polygon()
+
+
+    r21 = Rectangle(midpoint=(0.05, 0.), width=0.05, height=0.05).to_polygon()
+    r22 = Rectangle(midpoint=(0., 0.05), width=0.05, height=0.05).to_polygon()
+
+    transform = Periodic()
+    transform.set_periodicty("any", 0.1, 0.1)
+    topo.add(r11, level=1, transform=transform)
+    topo.add(r12, level=1, transform=transform)
+    topo.add(r21, level=2, transform=transform)
+    topo.add(r22, level=2, transform=transform)
+
+    return topo
+
+
+def generate_topo_football():
+    domain = Circle(midpoint=(0., 0.), radius = 1).discretize(refs=50)
+    topo = Topology(domain)
+
+    c1 = Circle(midpoint=(0., 0.), radius = 0.1).discretize(refs=6)
+    c2 = Circle(midpoint=(0.125, 0.125), radius = 0.1).discretize(refs=6)
+   
+
+    transform = Periodic()
+    transform.set_periodicty("any", 0.25, 0.25)
+    topo.add(c1, level=2, transform=transform)
+
+    topo.add(c2, level=1, transform=transform)
+
+
+    return topo
+
+
+
+
+if __name__ == "__main__":
+    topo = generate_topo_football()  # generate_topo_simple() #generate_topo()
+    topo.plot()
+    # gmsh_entities = topology_to_gmsh_entities(topo)
+    # write_geo(
+    #     gmsh_entities=gmsh_entities,
+    #     file_name="stellar_samples_periodic_constrains",
+    #     correct_curve_loops=True,
+    # )
